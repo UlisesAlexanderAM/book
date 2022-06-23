b1cfc04682e848565518cffbd25ea068349856d3
Ulises Alexander Arguelles Monjaraz <ulises_alexander_am@protonmail.com>
2022-06-23 17:39:31 -0500

    Add types and a function to ensure that the full name of a person has only letters.

--- src/Book.hs
+++ src/Book.hs
@@ -1,4 +1,32 @@
-module Book (someFunc) where
+module Book () where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Data.Char (isAlpha)
+import Data.Maybe (mapMaybe)
+import Prelude hiding (Word)
+
+newtype Letter = Letter Char deriving (Show)
+
+letter :: Char -> Maybe Letter
+letter c =
+  if isAlpha c
+    then Just $ Letter c
+    else Nothing
+
+letterString :: String -> [Letter]
+letterString = mapMaybe letter
+
+type Word = [Letter]
+
+newtype FirstName = FirstName Word deriving (Show)
+
+newtype LastName = LastName Word deriving (Show)
+
+data FullName
+  = OnlyName Word
+  | FullName (LastName, FirstName)
+  | FullNameM ([LastName], [FirstName])
+
+data Person
+  = Person FullName
+  | Alias String
+  | Anonymous
