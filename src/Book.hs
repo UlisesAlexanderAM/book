f0f5740d889f6b5043a54f4984fb9f2f34e29cd5
Ulises Alexander Arguelles Monjaraz <ulises_alexander_am@protonmail.com>
2022-06-24 23:10:01 -0500

    feat: create more types relevants for books

--- src/Book.hs
+++ src/Book.hs
@@ -2,11 +2,12 @@ module Book () where
 
 import Data.Char (isAlpha, isSpace)
 import Data.Maybe (mapMaybe)
+import Numeric.Natural (Natural)
 
 newtype Letters = Letters String
 
 instance Show Letters where
-    show (Letters c) = c
+  show (Letters c) = c
 
 letterString :: String -> Letters
 letterString = Letters . mapMaybe letter
@@ -20,26 +21,75 @@ letter c =
 newtype FirstName = FirstName Letters
 
 instance Show FirstName where
-    show (FirstName s) = show s
+  show (FirstName s) = show s
 
 newtype LastName = LastName Letters
 
 instance Show LastName where
-    show (LastName s) = show s
+  show (LastName s) = show s
 
-
-data FullName
-  = OnlyName Letters
-  | FullName (LastName, FirstName)
+newtype FullName = FullName (LastName, FirstName)
 
 instance Show FullName where
-    show (OnlyName a) = show a
-    show (FullName (a,b)) = show a ++ ", " ++ show b
-    
-stringToFullName :: (String,String) -> FullName
-stringToFullName (a,b)= FullName (LastName $ letterString a, FirstName $ letterString b)
+  show (FullName (a, b)) = show a ++ ", " ++ show b
+
+stringToFullName :: (String, String) -> FullName
+stringToFullName (a, b) = FullName (LastName $ letterString a, FirstName $ letterString b)
 
 data Person
-  = Person FullName
+  = PersonFullName FullName
   | Alias String
   | Anonymous
+
+instance Show Person where
+  show (PersonFullName (FullName (a, b))) = show a ++ ", " ++ show b
+  show (Alias s) = s
+  show Anonymous = "Anonymous"
+
+newtype Title = Title String
+
+instance Show Title where
+  show (Title s) = s
+
+newtype Language = Language [Letters]
+
+instance Show Language where
+  show (Language s) = show s
+
+newtype NoPages = NoPages Natural
+
+newtype NoWords = NoWords Natural
+
+newtype Editorial = Editorial String
+
+instance Show Editorial where
+  show (Editorial s) = s
+
+newtype Author = Author Person deriving (Show)
+
+newtype Translator = Translator Person
+
+instance Show Translator where
+  show (Translator s) = show s
+
+data BookFormat
+  = PhysicalBook
+  | DigitalBook
+
+instance Show BookFormat where
+  show PhysicalBook = "Physical Book"
+  show DigitalBook = "Digital Book"
+
+newtype Editor = Editor Person
+
+instance Show Editor where
+  show (Editor s) = show s
+
+newtype Ilustrator = Ilustrator Person
+
+instance Show Ilustrator where
+  show (Ilustrator s) = show s
+
+--TODO: create a tyoe for published date and publishing period
+--TODO: decide if use a enum type or a type class for categories and genres
+--TODO: decide how to aproach optionality before creating book type
