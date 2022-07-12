6c2bbb2e6c945528e6b7f2bc3606d40623e12a64
Ulises Alexander Arguelles Monjaraz <ulises_alexander_am@protonmail.com>
2022-07-11 19:35:36 -0500

    Reorganize types and add some new types

--- src/Book.hs
+++ src/Book.hs
@@ -84,65 +84,34 @@ newtype Ilustrator = Ilustrator Person
 
 newtype Title = Title String
 
-newtype Editorial = Editorial String
+newtype AlternativeTitle = AlternativeTitle Title
 
-newtype NoPages = NoPages Natural
+mkAlternativeTitle :: String -> AlternativeTitle
+mkAlternativeTitle = AlternativeTitle . Title
 
-newtype NoWords = NoWords Natural
+newtype Publisher = Publisher String
 
-data BookFormat
-  = Physical
-  | Digital
-  deriving (Show, Read)
+newtype Language = Language String
+
+newtype NumPages = NumPages Nat.Natural
+
+newtype NumWords = NumWords Nat.Natural
 
 newtype PubDate = PubDate Calendar.Day
 
 newtype PubPeriod = PubPeriod Calendar.CalendarDiffDays
 
-newtype PeriodSinceLastPubl = PeriodSinceLastPub Calendar.CalendarDiffDays
+newtype PeriodSinceLastPub = PeriodSinceLastPub Calendar.CalendarDiffDays
 
-class BookGenre a where
-  toGenre :: String -> a
-  fromGenre :: a -> String
+data ReadingStatus
+  = Reading
+  | Read
+  | WantedToRead
+  | Pause
+  | Dropped
 
-class BookCategory a where
-  toCategory :: String -> a
-  fromCategory :: a -> String
-
--- TODO: decide how to aproach optionality before creating book type
-{-
-ulisesArguelles :: Name
--- ulisesArguelles = FullName (LastName $ letterString "Arguelles", FirstName $ letterString "Ulises")
-ulisesArguelles = toName "Arguelles, Ulises"
-
--- fullNameUAAM = FullNameM (LastName $ letterString "Arguelles Monjaraz", FirstName $ letterString "Ulises Alexander")
-fullNameUAAM :: Name
-fullNameUAAM = toName "Arguelles Monjaraz, Ulises Alexander"
-
-testLetter :: IO ()
-testLetter = print $ toLetters "c"
-
-testLetters2 :: IO ()
-testLetters2 = print $ toLetters "sa54ss"
-
-testWord :: IO ()
-testWord = print $ toLetters "test"
-
-personUAAM1 :: Person
-personUAAM1 = mkPersonFullName "Ulises Alexander Arguelles Monjaraz"
-
-personUAAM2 :: Person
-personUAAM2 = mkPersonAlias "UAAM"
-
-personAnon :: Person
-personAnon = Anonymous
-
-testPerson1 :: IO ()
-testPerson1 = print personUAAM1
-
-testPerson2 :: IO ()
-testPerson2 = print personUAAM2
-
-testPerson3 :: IO ()
-testPerson3 = print personAnon
--}
+data PublishingStatus
+  = Publishing
+  | Finished
+  | Hiatus
+  | Cancelled
