d4c882a2f10d17d8d6a816fb689cef50315835a5
Ulises Alexander Arguelles Monjaraz <ulises_alexander_am@protonmail.com>
2022-10-18 23:04:06 -0500

    test: add some unit test
    - Add unit tests to ensure that Eq derivation works

--- test/Main.hs
+++ test/Main.hs
@@ -1,7 +1,22 @@
 module Main (main) where
 
+import Book (mkPersonName)
 import RIO
-import RIO.ByteString.Lazy (putStrLn)
+import Test.Tasty
+import Test.Tasty.HUnit
 
 main :: IO ()
-main = putStrLn "Test suite not yet implemented."
+main = defaultMain tests
+
+tests :: TestTree
+tests = testGroup "Tests" [unitTests]
+
+unitTests :: TestTree
+unitTests =
+  testGroup
+    "Unit Tests"
+    [ testCase "Two different person names are not equal" $
+        mkPersonName "Ulises" == mkPersonName "Alexander" @?= False,
+      testCase "Two equal person names are equal" $
+        mkPersonName "Ulises Alexander" == mkPersonName "Ulises Alexander" @?= True
+    ]
